db4d67b6a59589e6152315f835910cbd8225ae38
GoranKim0518 <109512293+GoranKim0518@users.noreply.github.com>
2024-11-22 16:10:44 +0900

    CSS, descriptions
    CSS, descriptions

---
+++ js/app.js
@@ -0,0 +1,102 @@
+//현재 위치 기반 자동 GPS 탐색 함수
+function success(pos) {
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+    console.log(lat);
+    console.log(lon);
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=ko&units=metric&appid=a60165ce2f175fe6914e08643827b2ef`;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let temperature = data.main.temp;
+            const cityName = data.name;
+            const weatherData = data.weather[0].main;
+            const tempText = document.querySelector(".main-temperature__text");
+            tempText.innerText = `${temperature.toFixed(1)}°C`;
+            const regionText = document.querySelector(".header__region");
+            regionText.innerText = cityName;
+            const weatherImg = document.getElementById("main-temperature__img");
+            console.log(`${cityName}: ${temperature}`);
+            console.log(weatherData);
+            switch(weatherData) {
+                case 'Clear':
+                    weatherImg.src = `imgs/sunny.png`;
+                    break;
+                case 'Clouds':
+                    weatherImg.src = `imgs/cloudy.png`;
+                    break;
+                case 'Rain':
+                    weatherImg.src = `imgs/rainy.png`;
+                    break;
+                case 'Snow':
+                    weatherImg.src = `imgs/.png`;
+                    break;
+                case 'Thunderstorm':
+                    weatherImg.src = `imgs/thunder.png`;
+                    break;
+            }
+        })
+        .catch((error) => {
+            return error;
+        });
+}
+
+function err() {
+    console.log("Coords Error!")
+}
+
+//직접 검색 기반 GPS 탐색 함수
+async function fetchFunc() {
+    const city = searchInput.value;
+    const urlDup = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ko&units=metric&appid=a60165ce2f175fe6914e08643827b2ef`;
+    try {
+        const res = await fetch(urlDup);
+        const data = await res.json();
+        console.log(data);
+        let temperatureDup = data.main.temp;
+        const weatherDataDup = data.weather[0].main;
+        const weatherImgDup = document.getElementById("main-temperature__img");
+        const tempTextDup = document.querySelector(".main-temperature__text");
+        tempTextDup.innerText = `${temperatureDup.toFixed(1)}°C`;
+        const regionTextDup = document.querySelector(".header__region");
+        regionTextDup.innerText = city;
+        switch(weatherDataDup) {
+            case 'Clear':
+                weatherImgDup.src = `imgs/sunny.png`;
+                break;
+            case 'Clouds':
+                weatherImgDup.src = `imgs/cloudy.png`;
+                break;
+            case 'Rain':
+                weatherImgDup.src = `imgs/rainy.png`;
+                break;
+            case 'Snow':
+                weatherImgDup.src = `imgs/.png`;
+                break;
+            case 'Thunderstorm':
+                weatherImgDup.src = `imgs/thunder.png`;
+                break;
+        }
+        console.log(`${city} :${temperatureDup}`);
+        searchInput.value="";
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+const searchInput = document.querySelector(".header__find-input");
+const searchInputIcon = document.querySelector(".header__find i");
+
+//이벤트 리스너
+searchInputIcon.addEventListener("click", function() {
+    searchInput.classList.toggle("display-hidden");
+    searchInput.focus;
+})
+
+searchInput.addEventListener("blur", function() {
+    searchInput.classList.add("display-hidden");
+})
+
+window.navigator.geolocation.getCurrentPosition(success, err);
+searchInputIcon.addEventListener("click", fetchFunc);
